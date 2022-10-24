a3a1c0c0812817dbfc3a351a2e5803fa1a5ba54f
Damian Perera <damianperera@outlook.com>
2022-10-24 19:25:51 +0200

    added electron app

---
+++ client/desktop/main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow, Menu} = require('electron')
+  const path = require('path')
+  const url = require('url')  
+
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 1024, height: 720, icon: __dirname + "icon.icns", label: "Chess.com"})
+  
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: 'chess.com',
+      protocol: 'https:',
+      slashes: true
+    }))
+  }
+  
+  app.whenReady().then(createWindow)
