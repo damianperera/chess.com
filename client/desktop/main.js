d38cb455903851841ec909486f2cf55d9d2bb308
Damian Perera <damianperera@outlook.com>
2022-10-25 07:05:37 +0200

    removed unnecessary icon declaration

--- client/desktop/main.js
+++ client/desktop/main.js
@@ -4,7 +4,7 @@ const {app, BrowserWindow, Menu} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 1024, height: 720, icon: __dirname + "icon.icns", label: "Chess.com"})
+    win = new BrowserWindow({width: 1024, height: 720, label: "Chess.com"})
   
     // and load the index.html of the app.
     win.loadURL(url.format({
